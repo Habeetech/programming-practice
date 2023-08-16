71ff4fe8dd5a31a799e46a7e61cd595ce6e9423f
Habeetech <oluhabnoni@gmail.com>
2023-08-16 02:56:39 -0700

    5 child

--- shell/5-child.c
+++ shell/5-child.c
@@ -4,4 +4,64 @@
 
 int main ()
 {
+	pid_t child;
+	int status;
+	char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
 
+	child = fork();
+	if (child == 0)
+	{
+		printf("I am the first child\n");
+		execve(argv[0], argv, NULL);
+		sleep(3);
+	}
+	else
+	{
+		wait(&status);
+	}
+	child = fork ();
+	if (child == 0)
+	{
+		printf("I am the second child\n");
+		execve(argv[0], argv, NULL);
+		sleep(3);
+	}
+	else
+	{
+		wait(&status);
+	}
+	child = fork();
+	if (child == 0)
+	{
+		printf("I am the third child\n");
+		execve(argv[0], argv, NULL);
+		sleep(3);
+	}
+	else
+	{
+		wait(&status);
+	}
+	child = fork();
+	if (child == 0)
+	{
+		printf("I am the fourth child\n");
+		execve(argv[0], argv, NULL);
+		sleep(3);
+	}
+	else
+	{
+		wait(&status);
+	}
+	child = fork();
+	if (child == 0)
+	{
+		printf("I am the fifth child\n");
+		execve(argv[0], argv, NULL);
+		sleep(3);
+	}
+	else
+	{
+		wait(&status);
+	}
+	return 0;
+}
