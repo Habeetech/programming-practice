b61a909973b2f6a0e3f6d63fbfafd18a84388254
Habeetech <oluhabnoni@gmail.com>
2023-08-17 04:47:51 -0700

    getenv

--- shell/getenv.c
+++ shell/getenv.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+
 char *_getenv(const char *name);
 
 char *_getenv(const char *name)
 {
-	unsigned int i;
-	while (name != NULL)
-	{
-		name[i];
-		i++;
-	}
-	name[i] = found;
-	return name;
+    extern char **environ; // Declare the global environ variable
+    for (int i = 0; environ[i] != NULL; i++) // Iterate through the environment variables
+    {
+        char *env_var = environ[i];
+        if (strncmp(name, env_var, strlen(name)) == 0 && env_var[strlen(name)] == '=') // Check if the name matches and is followed by '='
+        {
+            return &env_var[strlen(name) + 1]; // Return the value part of the environment variable
+        }
+    }
+    return NULL; // Environment variable not found
 }
-int main ()
+
+int main()
 {
-	char *value = _getenv("PATH");
-	if (value != NULL)
-	{
-		printf("PATH value is: %s\n", value);
-	}
-	else
-	{
-		printf("PATH value not found");
-	}
-	return 0;
+    char *value = _getenv("PATH");
+    if (value != NULL)
+    {
+        printf("PATH value is: %s\n", value);
+    }
+    else
+    {
+        printf("PATH value not found\n");
+    }
+    return 0;
 }
+
